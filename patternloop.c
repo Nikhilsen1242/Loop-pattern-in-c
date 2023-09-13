c5d2c13f4377739828dc18f3143ac5f37788e92c
Nikhilsen1242 <144876176+Nikhilsen1242@users.noreply.github.com>
2023-09-13 12:36:50 +0530

    Update patternloop.c

--- patternloop.c
+++ patternloop.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
-
 int main()
-
-{ int n,m,i,j,a=1,u,v,nn; scanf("%d", &n);
-
-m=2*n-1;
-
-for(i=1;i<=m;i++) {
-
-for(j=1;j<=m;j++) {
-
-if(i==a||j==a||i==m||j==m) printf("%2d",n); else
-
-{
-
-u=a;
-
-v=m;
-
-nn=n;
-
-loop:
+{ int n,m,i,j,a=1,u,v,nn; 
+ scanf("%d", &n);
+ m=2*n-1;
+for(i=1;i<=m;i++) 
+{  
+  for(j=1;j<=m;j++)
+    {  
+      if(i==a||j==a||i==m||j==m) 
+        printf("%2d",n);
+      else
+      {  
+        u=a;
+        v=m;
+        nn=n; 
+        loop:
+             u++;
+             V--;
+             nn--;
+        if(i==u||j==u||i==v||j==v)
+          printf("%2d", nn);
+        else
+        goto loop;
+      }
+    }
+  printf("\n");
+}
+ return 0;
+}
